c5395eb2f6746f31c626dd054734bc535212c2e5
Sean M <mctastic2121@gmail.com>
2022-05-23 14:19:26 -0400

    pushing working code

--- public/js/index.js
+++ public/js/index.js
@@ -1,43 +1,52 @@
 const btn = document.querySelector(".btn");
 const responseListEl = document.querySelector(".responseList");
 const promptTextEl = document.querySelector(".promptText");
-const promptResponseEl = document.querySelector(".promptResponse");
+const promptResponseEl = document.querySelector(".responseText");
 let responses = 0;
+let responseObj = {};
 
-const init = () =>{
-  if(responses===0){
+const init = () => {
+  if (responses === 0) {
     responseListEl.style.display = "none";
-  }else{
-  responseListEl.style.display= "flex";
+  } else {
+    responseListEl.style.display = "flex";
   }
-}
-const getResponse = ()=>
-fetch('/api', {
-  method: 'GET',
-  headers: {
-    'Content-Type': 'application/json'
-  }
-});
+};
+const getResponse = () => {
+  fetch("/api").then((response) =>
+    response
+      .json()
+      .then((data) => ({
+        data: data,
+        status: response.status,
+      }))
+      .then((res) => {
+        // console.log(res.data.output);
+        promptResponseEl.innerHTML = `<p>${res.data.output}</p>`
+      })
+  );
+};
 
 const apiCall = async () => {
-  const userInput = document.querySelector(".form-control").value;
+  const prompt = document.querySelector(".form-control").value;
   event.preventDefault();
   // console.log(userInput)
-  if (userInput.value === "") {
+  if (prompt.value === "") {
     alert("Please enter a prompt");
   } else {
     await fetch("/api", {
       method: "POST",
       headers: {
-        "content-type":"application/json",
+        "content-type": "application/json",
       },
-      body: JSON.stringify({userInput}),
+      body: JSON.stringify({ prompt }),
     });
   }
-  responseListEl.style.display= "flex";
-  promptTextEl.innerHTML = userInput;
-  responses++
+  responseListEl.style.display = "flex";
+  promptTextEl.innerHTML = prompt;
+  getResponse();
+  responses++;
 };
 
 btn.addEventListener("click", apiCall);
-init()
+init();
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 3000;
 const path = require("path");
 const got = require("got");
+const fs = require("fs");
 require("dotenv").config();
 
 app.use(express.json());
@@ -10,13 +11,13 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 
-
 const apiURL = "https://api.openai.com/v1/engines/davinci/completions";
+const destination = "./db/query.json";
 
-
-app.post("/api",async (req,res) => {
+app.post("/api", async (req, res) => {
   // console.log(req.body.userInput)
-  const prompt = req.body.userInput
+
+  const prompt = req.body.prompt;
   const url = apiURL;
   const params = {
     prompt: prompt,
@@ -27,7 +28,7 @@ app.post("/api",async (req,res) => {
     presence_penalty: 0.0,
   };
   const headers = {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
     Authorization: `Bearer ${process.env.OPENAI_SECRET}`,
   };
 
@@ -35,30 +36,30 @@ app.post("/api",async (req,res) => {
     const response = await got
       .post(url, { json: params, headers: headers })
       .json();
-    output = `${prompt}${response.choices[0].text}`;
+    output = `${response.choices[0].text}`;
     // console.log(output);
-    res.send(output)
+    const messageObj = {
+      output: output,
+    };
+    res.send(messageObj);
+    fs.writeFile(destination, JSON.stringify(messageObj, null, 4), (err) =>
+      err
+        ? console.error(err)
+        : console.info(`\nData written to ${destination}`)
+    );
   } catch (err) {
-    console.log("The error is"+err);
+    console.log("The error is " + err);
   }
 });
 
-// app.get("/api", async (req, res) => {
-//   try
-//    {
-//     const response = await axios.get(apiURL, {
-//       headers: {
-//         "Content-Type": "application/json",
-//         Authorization: `Bearer ${process.env.OPENAI_SECRET}`,
-//       },
-//       json: data
-//     });
-//     console.log(result);
-//     res.send(result.data);
-//   } catch (error) {
-//     console.log("THIS ERROR IS" + error);
-//   }
-// });
+app.get("/api", async (req, res) => {
+  try {
+    const data = await JSON.parse(fs.readFileSync("./db/query.json"));
+    res.send(data);
+  } catch (error) {
+    console.log("THIS ERROR IS" + error);
+  }
+});
 
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "/index.html")));
 
